a418f4a8c49d74ac8cd814cfd528c6fd45ddc721
Sebastian Glentworth <seb@stian.co.uk>
2017-12-21 10:08:05 +0000

    Add project files.

---
+++ Thickener.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Thickener.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Thickener/Thickener.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Thicken
+{
+    public static class Thickener
+    {
+        public static string Thicken(this string s){
+            s.ToCharArray();
+        }
+    }
+}
