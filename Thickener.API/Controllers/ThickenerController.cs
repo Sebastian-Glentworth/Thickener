a905f0e7726af654abf601a2c4b56791cce88caf
Sebastian Glentworth <seb@stian.co.uk>
2017-12-21 23:48:48 +0000

    added webapi project for thickening strings and renamed solution to "Thickener.sln"

---
+++ Thickener.API/Controllers/ThickenerController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thickener.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class ThickenerController
+    {
+        [HttpPost]
+        public string Thicken(string thinString)
+        {
+            return thinString.Thicken();
+        }
+    }
+}
